93bf6210f5c6ebeb36aa170fd5a9692e6d97b40e
tikowikoFamily <mrgamerdu84@gmail.com>
2026-08-14 15:41:05 +0200

    Détecte automatiquement les feux et intersections de la scène

--- Assets/Scripts/Traffic/MobileTrafficController.cs
+++ Assets/Scripts/Traffic/MobileTrafficController.cs
@@ -28,6 +28,18 @@ namespace Boulangerie3D.Traffic
 
         private void Awake()
         {
+            // Controls and reservation zones are inexpensive to discover once at startup.
+            // This prevents stale/missing Inspector references from making cars ignore
+            // traffic lights or intersections. There are no scene-wide searches per frame.
+            TrafficControlPoint[] discoveredControls = FindObjectsByType<TrafficControlPoint>(FindObjectsSortMode.None);
+            if (discoveredControls.Length > 0)
+                controls = discoveredControls;
+
+            TrafficIntersectionReservation[] discoveredIntersections =
+                FindObjectsByType<TrafficIntersectionReservation>(FindObjectsSortMode.None);
+            if (discoveredIntersections.Length > 0)
+                intersections = discoveredIntersections;
+
             vehicles = vehiclePoolRoot != null
                 ? vehiclePoolRoot.GetComponentsInChildren<TrafficVehicleAgent>(true)
                 : new TrafficVehicleAgent[0];
